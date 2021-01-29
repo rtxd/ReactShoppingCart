ee670f8487a6320de5f2b76c26dbfd0383a3c689
Alex <alejandro.munoz@student.uts.edu.au>
2021-01-29 15:56:29 +1100

    Add cart items to cart

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -41,7 +41,22 @@ const App = () => {
     return (items.reduce((ack: number, item) => ack + item.amount, 0));
   };
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      // 1. Is the item already in the cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        ))
+      }
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    })
+  };
 
   const handleRemoveFromCart = () => null;
 
