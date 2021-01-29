1ef44b7dcb1fef32b39606ca62e507878572d149
Alex <alejandro.munoz@student.uts.edu.au>
2021-01-29 13:45:32 +1100

    Created the Shop Item component and displayed it

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { useQuery } from 'react-query';
+import { useState } from "react";
+import { useQuery } from "react-query";
 // Components
-import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Grid from '@material-ui/core/Grid';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import Badge from '@material-ui/core/Badge';
+import Item from "./Item/Item";
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 // Styles
-import { Wrapper } from './App.styles'
+import { Wrapper } from "./App.styles";
 // Types
 export type CartItemType = {
   id: number;
@@ -19,16 +20,38 @@ export type CartItemType = {
   amount: number;
 };
 
-const getProducts = async (): Promise<CartItemType[]> => 
+const getProducts = async (): Promise<CartItemType[]> =>
   // The first await is for when we convert the return to JSON (converting to JSON is also Async)
   // The second await is for the API call itself
-  await (await fetch('https://fakestoreapi.com/products')).json();
-
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
   console.log(data);
-  return <div className="App">Start</div>
-}
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong ...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default App;
---
+++ react-shopping-cart/src/Item/Item.tsx
@@ -0,0 +1,26 @@
+import Button from '@material-ui/core/Button';
+// Types
+import { CartItemType } from '../App';
+// Styles
+import { Wrapper } from './Item.styles';
+
+type Props = { 
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+        </div>
+        {/* needs to be inline arrow function here because we need to give the prop of item */}
+        {/* If it wasn't an inline function then it would be triggered immediatly */}
+        <Button onClick={() => handleAddToCart(item)} >Add to cart</Button>
+    </Wrapper>
+)
+
+export default Item;
